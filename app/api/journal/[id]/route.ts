f3e89e0cd5127c331d2bc1f8541c4d05e7a1b594
Scott Burack <itsmescottb123@gmail.com>
2024-01-23 17:45:03 -0500

    auto update analysis

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,6 +1,7 @@
 import { analyze } from "@/utils/ai"
 import { getUserByClerkId } from "@/utils/auth"
 import { prisma } from "@/utils/db"
+import { revalidatePath } from "next/cache"
 import { NextResponse } from "next/server"
 
 export const PATCH = async (request, {params}) => {
@@ -32,7 +33,5 @@ export const PATCH = async (request, {params}) => {
     update: analysis
   })
 
-  console.log(updated)
-
-  return NextResponse.json({ data: updatedEntry })
+  return NextResponse.json({ data: { ...updatedEntry, analysis: updated } })
 }
